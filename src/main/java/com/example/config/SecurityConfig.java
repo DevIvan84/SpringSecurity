c007730246c8eff91394b09c827ccd2028a25e14
Ivan Flores <emmanuel.flores.dev84@gmail.com>
2023-11-13 18:28:25 -0600

    Se cambia version de spring a 3.0.6 debido a que algunas clases de spring security marcaban como deprecadas, se adiona metodo 2 de filter chain lambda y redireccionamiento con successHandler

--- src/main/java/com/example/config/SecurityConfig.java
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -5,14 +5,44 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    //Configuration One
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+//        return httpSecurity
+//                .authorizeHttpRequests()
+//                    .requestMatchers("/v1/index2").permitAll()
+//                    .anyRequest().authenticated()
+//                .and()
+//                .formLogin().permitAll()
+//                .and()
+//                .build();
+//    }
+
+    //Configutacion Two
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+
         return httpSecurity
+                .authorizeHttpRequests( auth -> {
+                    auth.requestMatchers("/v1/index2").permitAll();
+                    auth.anyRequest().authenticated();
+                })
+                .formLogin()
+                    .successHandler(successHandler())
+                    .permitAll()
+                .and()
                 .build();
+    }
+
+    public AuthenticationSuccessHandler successHandler() {
+        return ((request, response, authentication) -> {
+            response.sendRedirect("/v1/index");
+        });
     }
 }
